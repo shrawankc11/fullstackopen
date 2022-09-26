b2793c25496b641fff8f21de7c0ee12a48787567
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-09-26 23:44:26 +0545

    modified bmicalculator to handle error and take input from command line

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,14 +1,35 @@
+interface bmiValues {
+    value1: number;
+    value2: number;
+}
+
+const extractArgs = (args: string[]): bmiValues => {
+    return { value1: Number(args[2]), value2: Number(args[3]) };
+}
+
 const calculateBmi = (height: number, weight: number): string => {
+    if (isNaN(height) || isNaN(weight)) {
+        throw new Error('send both args as Numbers!')
+    }
     const heightInMeter: number = height / 100;
     const squareOfHeight: number = heightInMeter ** 2;
     const BMI = weight / squareOfHeight;
     if (BMI <= 18.4) {
-        return 'Abnormal (Under weight)';
+        return 'underweight';
     } else if (BMI >= 18.5 && BMI <= 24) {
         return 'Noraml (Healthy weight)';
-    } else if (BMI > 30 && BMI <= 40) {
-        return 'Abnormal (Over weight)';
+    } else if (BMI > 25 && BMI <= 40) {
+        return 'overweight';
     }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+    const { value1, value2 } = extractArgs(process.argv)
+    console.log(calculateBmi(value1, value2))
+} catch (err) {
+    let errorMessage: string = 'Something bad happened. ';
+    if (err instanceof Error) {
+        errorMessage += 'Error: ' + err.message
+    }
+    console.log(errorMessage)
+}
