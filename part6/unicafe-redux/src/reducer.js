5be5f76bb5beef6fd051e40c4ff6f9c9bbe56ea4
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-11 20:38:08 +0545

    completed exercise 6.1 - 6.2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -5,19 +5,17 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
-      return state
-    default: return state
+      return { ...state, good: 0, ok: 0, bad: 0 }
+    default: return initialState
   }
-  
 }
 
 export default counterReducer
