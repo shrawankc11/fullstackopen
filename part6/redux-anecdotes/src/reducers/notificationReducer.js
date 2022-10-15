37d13706ebf65df72ab97c0cfd6a6c6b94c67739
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-15 22:20:27 +0545

    added notification reducer for notifications

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = null;
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState,
+    reducers: {
+        createNotification(_state, action) {
+            return action.payload;
+        },
+    },
+});
+
+export const { createNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
