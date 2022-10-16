739070a4f5a28daafb3666627b938fb025c46c69
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-16 20:57:42 +0545

    added json-server to project

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,8 +2,18 @@ import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
+import { useEffect } from 'react';
+import { getAnecdotes } from './reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
+import anecdoteService from './services/anecdotes';
 
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        anecdoteService.getAll().then((anecdotes) => {
+            dispatch(getAnecdotes(anecdotes));
+        });
+    }, [dispatch]);
     return (
         <div>
             <h2>Anecdotes</h2>
