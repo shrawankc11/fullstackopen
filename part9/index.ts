20052fe71e408136e739ed89bd297adda0547aee
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-12-13 23:52:21 +0545

    new endpoint for exercise

--- part9/index.ts
+++ part9/index.ts
@@ -1,29 +1,72 @@
-import express = require('express');
+import express from 'express';
 import http = require('http');
-import { bmiCalculator } from './bmiCalculator';
+// import { bmiCalculator } from './bmiCalculator';
+import { calculateExercise, parseArgs } from './exerciseCalculator';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const server = http.createServer(app);
 
 app.get('/hello', (_req, res): object => {
     return res.send('Hello Full Stack!');
-})
+});
 
-app.post('/bmi', (req, res) => {
-    const { height, weight }: any = req.query;
-    try {
-        const result: string = bmiCalculator(height, weight);
-        res.send({ message: result });
-        return;
-    } catch (err) {
-        console.log(err.message)
-        res.send({
-            error: 'malformatted parameters'
-        })
+// app.get('/bmi', (req, res) => {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const { height, weight } = req.query;
+//     try {
+//         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+//         const result: string = bmiCalculator(height, weight);
+//         res.send({ message: result });
+//         return;
+//     } catch (err: unknown) {
+//         console.log(err.message);
+//         res.send({
+//             error: 'malformatted parameters'
+//         });
+//     }
+// });
+
+app.post('/exercise', (req, res) => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target } = req.body;
+
+    if (!daily_exercises || !target) {
+        return res.json({
+            error: 'parameter missing'
+        });
     }
-})
+    try {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        parseArgs(daily_exercises);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        const result = calculateExercise(daily_exercises, target);
+        return res.json({
+            message: result
+        });
+    } catch (err) {
+        return res.send({
+            error: "malformatted parameters"
+        });
+    }
 
-const PORT: number = 3000;
+
+});
+
+/*app.post('/calculate', (req, res) => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment 
+    const { val1, val2, op }: any = req.body;
+
+    if (!val1 || isNaN(Number(val1))) {
+        return res.status(400).send({ error: "Provide both values as numbers." });
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator(Number(val1), Number(val2), op);
+    res.send(result);
+});*/
+
+const PORT = 3000;
 server.listen(PORT, () => console.log(`server listening at ${PORT}`));
